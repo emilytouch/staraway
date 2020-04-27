78aad838141c6b18033f3df3bd38cf2bfb463568
emilytouch <emilytovch@gmail.com>
2020-04-27 17:27:58 -0400

    Making repo just build

--- CharactersAndStuff/Maps/hyptosis_tile-art-batch-4.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.3" name="hyptosis_tile-art-batch-4" tilewidth="32" tileheight="32" tilecount="900" columns="30">
- <image source="../../EmilyTouch_SeniorDesign/Assets/Sprites/maps/hyptosis_tile-art-batch-4.png" trans="ff00ff" width="960" height="960"/>
-</tileset>
--- CharactersAndStuff/Maps/hyptosis_tile-art-batch-5.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.3" name="hyptosis_tile-art-batch-5" tilewidth="32" tileheight="32" tilecount="722" columns="19">
- <image source="../../EmilyTouch_SeniorDesign/Assets/Sprites/maps/hyptosis_tile-art-batch-5.png" trans="ff00ff" width="636" height="1219"/>
-</tileset>
